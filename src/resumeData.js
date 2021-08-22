3c06814de53489d9c638d3549544540e4dbbd167
neezystudio <victorneez6@gmail.com>
2021-08-22 19:11:11 +0300

    space

---
+++ src/resumeData.js
@@ -0,0 +1,129 @@
+let resumeData = {
+    "imagebaseurl":"https://scontent.fnbo10-1.fna.fbcdn.net/v/t1.6435-9/179521683_2195669100569640_5537529312021782363_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=OiopOt9g3XwAX_FtHUD&_nc_pt=5&_nc_ht=scontent.fnbo10-1.fna&oh=c87ec43e4996c8983cbb75ede7010791&oe=6144083F",
+    "name": "Victor Ndirangu",
+    "role": "Full Stack Developer and Data Scientist, As a self-taught developer, there’s no one to show me the ropes.",
+    "linkedinId":"Victor Ndirangu",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of software development and like to learn about new technologies, write technical articles or simply play music in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/victor-ndirangu-911420134/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/neezystudio",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"Twitter",
+          "url":"https://twitter.com/home",
+          "className":"fa fa-twitter"
+        },
+        "name":"Instagram",
+          "url":"https://www.linkedin.com/in/victor-ndirangu-911420134/",
+          "className":"fa fa-linkedin"
+      ],
+    "aboutme":"I am currently a full-time Developer in Nairobi Kenya and pursuing my B.Tech from here. I am a self taught Full Stack mobile & Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Nairobi, Kenya",
+    "phone": "+254707461491",
+    
+    "education":[
+      {
+        "UniversityName":"Muranga University of Technology",
+        "specialization":"Computer Software and System Programming",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Bachelors in Software Engineering"
+      },
+      // {
+      //   "UniversityName":"Some University",
+      //   "specialization":"Some specialization",
+      //   "MonthOfPassing":"Jan",
+      //   "YearOfPassing":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "work":[
+      {
+        "CompanyName":"Samchi Group, Nairobi Kenya",
+        "specialization":"Lead Software Engineer",
+        "MonthOfLeaving":"Present",
+        "YearOfLeaving":"Present",
+        "Achievements":"I have spearheaded the establishment of the start-up SamchiSystems&Solutions and currently inveolved in Real Estate Projects in the same company"
+      },
+      {
+        "CompanyName":"CrowdBotics, Carlifonia USA",
+        "specialization":"Freelance developer with React Native & ReactJs with django framework",
+        // "MonthOfLeaving":"Jan",
+        // "YearOfLeaving":"2018",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Gemini Global Markets, Nairobi Kenya",
+        "specialization":"Graphic Designer",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Dart"
+      },
+      {
+        "skillname":"Flutter"
+      },
+      {
+        "skillname":"React Native"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Python with Django Framework"
+      }
+    ],
+    "portfolio":[
+       {
+        "name":"Samchi Systems",
+        "description":"Start-up Tech company",
+        "imgurl":"https://res.cloudinary.com/emacon-production/image/upload/v1627376803/Nairobarry/Samchi_Systems_2_B_dy8zvf.jpg"
+      },
+      {
+        "name":"Tukio",
+        "description":"An event ecosystem mobile app",
+        "imgurl":"images/Tukio.jpg"
+      },
+      {
+        "name":"Exirgy",
+        "description":"Amobile app technology based under SAP technology",
+        "imgurl":"https://avatars.githubusercontent.com/u/88222102?s=400&u=c89cc902f3162c57e462fd6251fedb68fb270556&v=4"
+      },
+      {
+        "name":"Shikilia Ecommerce",
+        "description":"A 3 server side e-commerce multivendor web-app",  
+        "imgurl":"https://res.cloudinary.com/emacon-production/image/upload/v1623146015/Shikilia/Shikilia_Stores_Logo_zdzer3.png"
+      },
+      {
+        "name":" Nairoberry",
+        "description":"A movie streaming site for the Nairoberry Movie from Avant Tv",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"An accomplished and skillful developer who always deliver on time and can be relied on. He is relaxed, good to work with and listens to his clients needs first We have been working together for two years now, firstly I hired him for a project and he did such an amazing job that we began to use him for more projects later on. His skills include Java, WooCommerce, Wordpress, NodeJS and PHP",
+
+        "name":"Group General Manager Samchi Group"
+      },
+      {
+        "description":"I was impressed by Victor’s prices, especially for the project I wanted to do and in comparison to the quotes I received from a lot of other companies",
+        "name":"General Manager Shikilis Stores"
+      }
+    ]
+  }
+  
+  export default resumeData
